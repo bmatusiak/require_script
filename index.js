d6139ebb5be6987e3f18d1d490e740b7f215afe0
Bradley Matusiak <bmatusiak@gmail.com>
2024-08-18 09:46:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 
-module.exports = (function (global_object) {
+module.exports = (function () {
+
+    const global_object = (() => {
+        if (process.__nwjs) {
+            return global;
+        } else {
+            return global;
+        }
+    })();
+
     var { createRequire } = require("module");
     var fs = require("fs");
     var path = require("path");
@@ -25,10 +34,14 @@ module.exports = (function (global_object) {
     }
     function parseBabel(src) {
         var contents = fs.existsSync(src) && fs.readFileSync(src).toString("utf8");
-        var opts = require_script.babel || {
+        var srcPath = src;
+        if(process.__nwjs){
+            srcPath = path.relative(path.dirname(path.normalize(process.cwd()+window.location.pathname)),src);
+        }
+        var opts = requireScript.babel || {
             filename: path.basename(src),
             sourceMaps: "inline",
-            sourceFileName: src
+            sourceFileName: srcPath
         };
         opts.plugins = ["@babel/plugin-transform-modules-umd"];
         opts.presets = [];
@@ -43,6 +56,7 @@ module.exports = (function (global_object) {
     function useBabel(realPath) {
         var useBabel = false;
         switch (path.extname(realPath)) {
+            case ".js":
             case ".jsx":
             case ".ts":
             case ".tsx":
@@ -51,41 +65,62 @@ module.exports = (function (global_object) {
             default:
                 useBabel = false;
         }
+
         return useBabel;
     }
-    function require_script($require) {
-        return function require_script(src_location) {
-            const realPath = $require.resolve(src_location);
+    function requireScript($require, request_dir, request_file) {
+        console.log(request_dir, request_file)
+        var basePath = path.dirname(path.resolve(request_dir, request_file));
+        require_script.basepath = (path) => { basePath = path; };
+        function require_script(src_location) {
+            if (!(src_location[0] == ".")) return $require(src_location);
+            var realPath = path.resolve(basePath, src_location);
+            const script_dir = path.dirname(realPath);
+            const script_name = path.basename(realPath);
+
+            if (path.extname(realPath) == ".json") { return JSON.parse(fs.readFileSync(realPath)); }
+            if (realPath == src_location) return $require(src_location);
             const newRequire = createRequire(realPath);
             var new_require = function (src) {
-                if (useBabel(src) && src[0] == ".")
-                    return require_script(newRequire)(src);
-                else return newRequire(src);
+                var rs = requireScript(newRequire, script_dir, script_name);
+                return rs(src);
+            };
 
-            }
             global_object.exports = {};
             global_object.module = { exports: global_object.exports };
             var module = global_object.module;
+
             var oldRequire = global_object.require;
             global_object.require = new_require;
+
+            var old_dirname = global_object.__dirname;
+            global_object.__dirname = script_dir;
+            var old_filename = global_object.__filename;
+            global_object.__filename = script_name;
+
             var output = useBabel(src_location) ? parseBabel(realPath) : fs.readFileSync(realPath);
             function requireWrap(src) {
-                return "(function (require){" + src + "\n})(require);"
+                return "(function (require,__dirname, __filename, module){" + src + "\n})(global.require,global.__dirname,global.__filename, global.module);";
             }
             vm.runInThisContext(requireWrap(output), realPath);
             if (module && module.exports) {
                 module.script = src_location;
             }
             global_object.require = oldRequire;
+
+            global_object.__dirname = old_dirname;
+            global_object.__filename = old_filename;
+
             global_object.exports = {};
             global_object.module = { exports: global_object.exports };
+            // if (!module.exports.default) module.exports.default = module.exports;
+
+            console.log(realPath);
+
             return module.exports;
         }
+        return require_script;
     }
-    return require_script;
+    return requireScript;
 
-})(
-    typeof window != "undefined" ? window :
-        typeof globalThis != "undefined" ? globalThis :
-            typeof global != "undefined" ? global :
-                false);
+})();

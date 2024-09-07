7eabe0d95c6bb8282a2ac5ebd59eb7781c945468
Bradley Matusiak <bmatusiak@gmail.com>
2024-09-07 18:32:12 -0400

    update

--- react.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    if (typeof module != "undefined") {
-        const React = require("react");
-        const { createRoot } = require("react-dom/client");
-        React.createRoot = createRoot;
-        module.exports = React;
-    }
-})();
